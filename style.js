90fd390b7ba9cc062a2543ce228ecfe8c3a784c9
LizhenZhu <zlz19961108@gmail.com>
2019-01-13 23:38:55 -0500

    initialization

---
+++ style.js
@@ -0,0 +1,11 @@
+var options = {
+  strings: ['   I am Lizhen Zhu ', 'I am an Interaction Designer ','I am an User Experience Designer', 'imbuing empathy and perspective through experience and interaction design'],
+  typeSpeed: 100, 
+  startDelay: 500, 
+  backSpeed: 50, 
+  backDelay: 900, 
+  loop: false, 
+  showCursor: false,
+  cursorChar: "|",
+}
+ityped.init('#typewords', options); 
